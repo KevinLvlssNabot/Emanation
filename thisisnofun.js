acef93cfbffd3c69d387b728cbd0fe31e47d85ab
LunaSimoes <>
2020-02-10 15:49:50 +0100

    Setup

---
+++ thisisnofun.js
@@ -0,0 +1,41 @@
+var config = {
+	type: Phaser.AUTO,
+	width: 800,
+	height: 600,
+physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 600 },
+            debug: true
+        }
+    },
+scene: {
+		init: init,
+		preload: preload,
+		create: create,
+		update: update
+	}
+};
+
+var game = new Phaser.Game(config);
+
+function init(){
+
+}
+
+function preload(){
+
+}
+
+
+
+function create(){
+
+	}
+
+
+
+
+function update(){
+
+}
