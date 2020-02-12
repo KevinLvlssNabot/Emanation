3c2c5b2cce5a262d51693f0b9e532e2b993960ee
KevinLvlssNabot <59653290+KevinLvlssNabot@users.noreply.github.com>
2020-02-12 10:51:10 +0100

    enigmeK

---
+++ Enigmes/EnigmeKevin/enigmeKevin.js
@@ -0,0 +1,13 @@
+let boutons = $('.boutons')
+let suivant = $('#suivant')
+let dialBox = $('.dialBox')
+let charaName = $('.charaName')
+
+
+
+
+suivant.on('click', function(){
+  dialBox.css("visibility","hidden");
+  charaName.css("visibility","hidden");
+  
+});
